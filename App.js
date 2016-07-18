c9801527a6fc3c899347441730fa7fce1aff9587
dannycrews <dancrews@me.com>
2016-07-17 22:14:09 -0400

    Snag some React snippets

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+
+// class component
 class App extends React.Component {
   render(){
-    return <div>Hi</div>
+    return (
+    <div>
+    <h1>Hello World</h1>
+    <bold>Bold</bold>
+    </div>
+    );
   }
 }
+
+// stateless function component
+//const App = () => <h1>Hello Eggheads</h1>
+
 export default App;
