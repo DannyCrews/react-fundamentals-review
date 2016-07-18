0c8d9cce43a7624d877200e078fbaa558253a8fc
dannycrews <dancrews@me.com>
2016-07-17 22:53:57 -0400

    state basics

--- App.js
+++ App.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 // class component
 class App extends React.Component {
+  constructor(){
+    super();  // gives us our context for 'this' wihtin our component
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    }
+  }
+
+  update(e){
+    this.setState({txt: e.target.value});
+  }
+
+
   render(){
-    let txt = this.props.txt;
     return (
-    <h1>{txt}</h1>
+    <div>
+      <input type='text'
+        onChange={this.update.bind(this)} />
+      <h1>{this.state.txt}</h1>
+    </div>
     );
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: "this is the default text"
-}
-
-
-
 // stateless function component
 //const App = () => <h1>Hello Eggheads</h1>
 
-ReactDOM.render(
-  <App cat={5}  />,
-  document.getElementById('app')
-);
-
+export default App;
