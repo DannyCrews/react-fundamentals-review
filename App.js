73ff6314d593a0399acf5cb1c020408ca16aa17b
dannycrews <dancrews@me.com>
2016-07-17 22:31:28 -0400

    playing with props

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // class component
 class App extends React.Component {
   render(){
+    let txt = this.props.txt;
     return (
-    <div>
-    <h1>Hello World</h1>
-    <bold>Bold</bold>
-    </div>
+    <h1>{txt}</h1>
     );
   }
 }
 
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: "this is the default text"
+}
+
+
+
 // stateless function component
 //const App = () => <h1>Hello Eggheads</h1>
 
-export default App;
+ReactDOM.render(
+  <App cat={5}  />,
+  document.getElementById('app')
+);
+
